f29839586b280bd0e39d2706ac90765fefbeacec
Magnus Werner Ottesen <magnuswernerottesen@gmail.com>
2025-04-09 12:53:41 +0200

    create accordion component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import styles from "./App.module.css";
 import Counter from "./components/Counter/Counter";
 import Input from "./components/Input/Input";
+import AccordionItem from "./components/AccordionItem/AccordionItem";
+import Accordion from "./components/Accordion/Accordion";
 
 function App() {
   return (
     <div className={styles.rootContainer}>
       {/* <Counter /> */}
-      <Input
+      {/* <Input
         placeHolder="Enter firstname"
         label="First name"
         errorMessage="First name is required"
-      ></Input>
+      ></Input> */}
+
+      <Accordion></Accordion>
     </div>
   );
 }
